78d2e1848922f2303153706c366c68c8cb200af2
EmerichRenan\User <renan.emerich1@gmail.com>
2025-07-08 20:38:14 -0300

    Adicionando nova aula

---
+++ (un)pack_tuplas.py
@@ -0,0 +1,18 @@
+# Introdução ao desempacotamento + tuplas (tuples)
+
+#Pack
+nomes = ['Maria', 'José', 'Pedro']
+nome1, nome2, nome3 = nomes
+print(nome1)
+
+# Unpack
+# O underscore age como uma variável não utilizável, então usamos para servir de complementação
+_, nome_2, *_ = ['João', 'Renan', 'Lucas']
+print(nome_2)
+
+# Tupla. É uma lista imutável.
+
+nomes = ['Ana', 'Clara', 'Renan']
+nomes = tuple(nomes) # Ou não utilizar [] na variavel que já cria uma tuple sem usar o método tuple()
+
+print(nomes[1])
