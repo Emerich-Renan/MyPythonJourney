4e7243ceeaa18d3c49dac16665785abd0068b253
EmerichRenan\User <renan.emerich1@gmail.com>
2025-07-12 18:48:45 -0300

    Renomeando arquivos para melhor organização

---
+++ básicos/1_imc.py
@@ -0,0 +1,17 @@
+nome = 'Renan'
+sobrenome = 'Emerich'
+idade = 19
+ano_nascimento = 2005
+maior_de_idade = idade >= 18
+altura_metros = 1.80
+peso = 80
+imc = peso / altura_metros ** 2
+
+# print('Nome:', nome)
+# print('Sobrenome:', sobrenome)
+# print('Idade:', idade)
+# print('Ano de nascimento:', ano_nascimento)
+# print('É maior de idade?:', maior_de_idade)
+# print('Altura em metros:', altura_metros)
+
+print(f'{nome} tem {altura_metros} de altura, pesa {peso} quilos e seu imc é {imc}')
