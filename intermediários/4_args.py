67a442284a719a8d96ffd72d631f88e28a23062d
EmerichRenan\User <renan.emerich1@gmail.com>
2025-07-12 21:08:58 -0300

    Adicionando nova aula

---
+++ intermediários/4_args.py
@@ -0,0 +1,26 @@
+"""
+  args - Argumentos não nomeados
+  * - *args (empacotamento e desenpacotamento)
+"""
+# Lembre-te de desempacotamento
+# x, y, *resto = 1, 2, 3, 4, 5
+# print(x, y, resto)
+
+# def soma(x, y):
+#   return x + y 
+
+def soma(*args):
+  total = 0
+  for numero in args:
+    total += numero
+  return total
+  
+numeros = 1, 2, 3, 4, 5, 6, 7
+
+print(sum(numeros))
+
+print(*numeros)
+
+outra_soma = soma(*numeros)
+print(soma(*numeros))
+print(outra_soma)
