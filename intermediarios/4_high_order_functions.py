aa8d7b61242c91645ae612fcce594d8dfd52f68b
EmerichRenan\User <renan.emerich1@gmail.com>
2025-07-13 00:35:20 -0300

    Atualizando conteúdo de aula

--- intermediarios/4_high_order_functions.py
+++ intermediarios/4_high_order_functions.py
@@ -37,3 +37,15 @@ print(
 print(
     executa(saudacao, 'Bom dia', 'Clara')
 )
+
+"""
+Termos técnicos: Higher Order Functions e First-Class Functions
+
+Academicamente, os termos Higher Order Functions e First-Class Functions têm significados diferentes.
+
+Higher Order Functions - Funções que podem receber e/ou retornar outras funções
+
+First-Class Functions - Funções que são tratadas como outros tipos de dados comuns (strings, inteiros, etc...)
+
+Observação: esses termos podem ser diferentes e ainda refletir o mesmo significado.
+"""
